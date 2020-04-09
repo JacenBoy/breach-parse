bdf7639d55829f88ba1b3ca1252ac1f069e81c99
JacenBoy <jacenboy1@gmail.com>
2020-04-09 11:34:52 -0400

    Reference compilation's base directory instead of /data

--- breach-parse.sh
+++ breach-parse.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# assume default location
+breachDataLocation="/opt/breach-parse/BreachCompilation"
+
 if [ $# -lt 2 ]; then
     echo "Breach-Parse v2: A Breached Domain Parsing Tool by Heath Adams"
     echo " "
     echo "Usage: ./breach-parse.sh <domain to search> <file to output> [breach data location]"
     echo "Example: ./breach-parse.sh @gmail.com gmail.txt"
-    echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
-    echo "You only need to specify [breach data location] if its not in the expected location (/opt/breach-parse/BreachCompilation/data)"
+    echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation"'
+    echo "You only need to specify [breach data location] if its not in the expected location (${breachDataLocation})"
     echo " "
     echo 'For multiple domains: ./breach-parse.sh "<domain to search>|<domain to search>" <file to output>'
     echo 'Example: ./breach-parse.sh "@gmail.com|@yahoo.com" multiple.txt'
@@ -18,23 +21,21 @@ else
         exit 1
     fi
 
-    # assume default location
-    breachDataLocation="/opt/breach-parse/BreachCompilation/data"
     # check if BreachCompilation was specified to be somewhere else
     if [ $# -eq 3 ]; then
         if [ -d "$3" ]; then
             breachDataLocation="$3"
         else
             echo "Could not find a directory at ${3}"
-            echo 'Pass the BreachCompilation/data directory as the third argument'
-            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
+            echo 'Pass the BreachCompilation directory as the third argument'
+            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation"'
             exit 1
         fi
     else
         if [ ! -d "${breachDataLocation}" ]; then
             echo "Could not find a directory at ${breachDataLocation}"
-            echo 'Put the breached password list there or specify the location of the BreachCompilation/data as the third argument'
-            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
+            echo 'Put the breached password list there or specify the location of the BreachCompilation as the third argument'
+            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation"'
             exit 1
         fi
     fi
@@ -49,7 +50,7 @@ else
     touch $master
     # count files for progressBar
     # -not -path '*/\.*' ignores hidden files/directories that may have been created by the OS
-    total_Files=$(find "$breachDataLocation" -type f -not -path '*/\.*' | wc -l)
+    total_Files=$(find "$breachDataLocation/data" -type f -not -path '*/\.*' | wc -l)
     file_Count=0
 
     function ProgressBar() {
@@ -66,7 +67,7 @@ else
     }
 
     # grep for passwords
-    find "$breachDataLocation" -type f -not -path '*/\.*' -print0 | while read -d $'\0' file; do
+    find "$breachDataLocation/data" -type f -not -path '*/\.*' -print0 | while read -d $'\0' file; do
         grep -a -E "$1" "$file" >>$master
         ((++file_Count))
         ProgressBar ${number} ${total_Files}
